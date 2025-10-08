7fcaae3975d7007b634c54c3872ab648aa7ec8cd
eloritzkovitz <elor2200@gmail.com>
2025-10-08 13:02:43 +0300

    Updated theme

--- src/components/common/SearchInput.tsx
+++ src/components/common/SearchInput.tsx
@@ -19,7 +19,7 @@ export function SearchInput({
         onChange={(e) => onChange(e.target.value)}
         placeholder={placeholder}
         aria-label={placeholder || "Search"}
-        className={`px-3 py-2 bg-gray-100 rounded border border-none text-base focus:outline-none focus:ring-none ${className}`}
+        className={`filter-select px-3 py-2 bg-gray-100 rounded border border-none text-base focus:outline-none focus:ring-none ${className}`}
       />
     </div>
   );
