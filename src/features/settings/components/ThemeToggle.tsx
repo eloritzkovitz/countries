7f8d7c962fe9a17f63a6795da44b549e3fc51ed6
eloritzkovitz <elor2200@gmail.com>
2025-10-22 16:45:41 +0300

    Updated settings styles

---
+++ src/features/settings/components/ThemeToggle.tsx
@@ -0,0 +1,33 @@
+import { FaSun, FaMoon } from "react-icons/fa";
+
+type ThemeToggleProps = {
+  theme: "light" | "dark";
+  toggleTheme: () => void;
+};
+
+export function ThemeToggle({ theme, toggleTheme }: ThemeToggleProps) {
+  return (
+    <button
+      id="theme-toggle"
+      onClick={toggleTheme}
+      aria-label={`Switch to ${theme === "light" ? "dark" : "light"} theme`}
+      title={`Switch to ${theme === "light" ? "dark" : "light"} theme`}
+      className="theme-toggle-btn"
+    >
+      <span
+        className={
+          "theme-toggle-thumb " +
+          (theme === "dark"
+            ? "theme-toggle-thumb-dark"
+            : "theme-toggle-thumb-light")
+        }
+      >
+        {theme === "dark" ? (
+          <FaMoon className="text-white" />
+        ) : (
+          <FaSun className="text-gray-900" />
+        )}
+      </span>
+    </button>
+  );
+}
