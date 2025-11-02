4d2d7f837eb4723ab140eabe58266d385f72447d
eloritzkovitz <elor2200@gmail.com>
2025-11-02 21:44:20 +0200

    Map page is now lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,52 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Suspense, lazy } from "react";
+import { Routes, Route } from "react-router-dom";
+import { SplashScreen } from "@components";
 import { CountryDataProvider } from "@contexts/CountryDataContext";
 import { MapUIProvider } from "@contexts/MapUIContext";
 import { MarkersProvider } from "@contexts/MarkersContext";
 import { OverlayProvider } from "@contexts/OverlayContext";
-import { SettingsProvider } from "@contexts/SettingsContext";
+import { useSettings } from "@contexts/SettingsContext";
 import { TripsProvider } from "@contexts/TripsContext";
 import { UIProvider } from "@contexts/UIContext";
-import AtlasPage from "./pages/AtlasPage";
 import GamesPage from "./pages/GamesPage";
 import TripsPage from "./pages/TripsPage";
 
 function App() {
+  const { loading } = useSettings();
+
+  // Lazy load AtlasPage
+  const AtlasPage = lazy(() => import("./pages/AtlasPage"));
+
+  // Show splash screen while loading settings
+  if (loading) {
+    return <SplashScreen />;
+  }
+
   return (
-    <SettingsProvider>
-      <CountryDataProvider>
-        <TripsProvider>
-          <UIProvider>
-            <BrowserRouter>
-              <Routes>
-                <Route
-                  path="/"
-                  element={
-                    <OverlayProvider>
-                      <MapUIProvider>
-                        <MarkersProvider>
-                          <AtlasPage />
-                        </MarkersProvider>
-                      </MapUIProvider>
-                    </OverlayProvider>
-                  }
-                />
-                <Route path="/game" element={<GamesPage />} />
-                <Route path="/trips" element={<TripsPage />} />
-              </Routes>
-            </BrowserRouter>
-          </UIProvider>
-        </TripsProvider>
-      </CountryDataProvider>
-    </SettingsProvider>
+    <CountryDataProvider>
+      <TripsProvider>
+        <UIProvider>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Suspense fallback={<SplashScreen />}>
+                  <OverlayProvider>
+                    <MapUIProvider>
+                      <MarkersProvider>
+                        <AtlasPage />
+                      </MarkersProvider>
+                    </MapUIProvider>
+                  </OverlayProvider>
+                </Suspense>
+              }
+            />
+            <Route path="/game" element={<GamesPage />} />
+            <Route path="/trips" element={<TripsPage />} />
+          </Routes>
+        </UIProvider>
+      </TripsProvider>
+    </CountryDataProvider>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './styles/index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { SettingsProvider } from "@contexts/SettingsContext";
+import App from "./App";
+import "./styles/index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <SettingsProvider>
+        <App />
+      </SettingsProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
