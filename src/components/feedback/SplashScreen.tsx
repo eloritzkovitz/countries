730c9904ef49445e377e4404d1ed9696cc07fe4d
eloritzkovitz <elor2200@gmail.com>
2025-11-02 20:57:10 +0200

    Updated splash screen

--- src/components/feedback/SplashScreen.tsx
+++ src/components/feedback/SplashScreen.tsx
@@ -4,8 +4,8 @@ import "./SplashScreen.css";
 export function SplashScreen() {
   return (
     <div className="fixed inset-0 z-[1000] flex flex-col items-center justify-center bg-gray-100 dark:bg-gray-900 bg-opacity-80">
-      <div className="mb-10 flex items-center justify-center animate-logo-tilt">
-        <Branding size={128} />
+      <div className="mb-10 flex items-center justify-center">
+        <Branding size={156} />
       </div>
       <div className="flex gap-2 mt-2">
         <span
