5f04e8a9768f9698a2a796d8d61faebc7f845af9
eloritzkovitz <elor2200@gmail.com>
2025-11-05 08:30:25 +0200

    Updated timeline buttons

--- src/features/map/index.ts
+++ src/features/map/index.ts
@@ -1,11 +1,13 @@
 // Components
 export { MapToolbar } from "./toolbar/MapToolbar";
-export { TimelinePicker } from "./components/TimelinePicker";
 export { WorldMap } from "./components/WorldMap";
 
 // Hooks
 export { useMapGeographyStyle } from "./hooks/useMapGeographyStyle";
 export { useMapView } from "./hooks/useMapView";
+
+// Timeline
+export { TimelinePicker } from "./timeline/TimelinePicker";
 
 // Utils
 export * from "./utils/map";
